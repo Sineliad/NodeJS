1a1c1a74f0f78da036e31502d5b2f9b0bcae2cc1
Sineliad <sineliad15@gmail.com>
2021-07-05 16:44:55 -0600

    Node HTTP Example 2

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res)=>{
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html> <body> <h1> Hello word node http server. </h1> </body> </html>');
+    console.log("req.url",req.url);
+    console.log("req.method",req.method);
+    if(req.method == 'GET'){
+        var fileUrl;
+        if(req.url == '/'){
+            fileUrl= '/index.html'
+        }else{
+            fileUrl= req.url;
+        }
+        console.log("file url");
+        var filePath = path.resolve('./public'+fileUrl);
+        console.log("file path: ", filePath);
+        const fileExt = path.extname(filePath);
+        console.log("fileExt: ", fileExt);
+        if(fileExt == '.html'){
+            fs.exists(filePath, (exist)=> {
+                if(!exist){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                    ' not found</h1></body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ' + req.method + 
+                    ' not supported</h1></body></html>');
+        }
+    }
 });
 
 server.listen(port, hostname, () => {
